d130a04793cfca44c5f91c8cb8c0a6d3419cce0a
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-08-11 16:03:40 +0300

    Send emails using a template html

--- email_sender.py
+++ email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Emanuel Ciuca'
 email['to'] = 'emanuel.ciuca@gmail.com'
 email['subject'] = 'You won a million dollars'
 
-email.set_content('I am a Python Master!')
+email.set_content(html.substitute({'name': 'TinTin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
